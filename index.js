ac6b29636487268ea3f613830a6c8f273c698c91
Aneesh Venuturumilli <veeaneesh@gmail.com>
2018-12-28 04:13:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,17 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
-  var empty = '';
+  var empty = ["In your cart, you have "];
+  if(cart.length===0){
+    return "Your shopping cart is empty."
+  }
+  for(var i =0;i<cart.length;i++){
+    if(i===cart.length-1){
+      empty.push(`${cart[i]["itemName"]} at $${cart[i]["itemPrice"]}.`)
+    }else{
+      
+    }
+  }
   
 }
 
